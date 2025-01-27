2dd05a5cfbc6e6a48208294bc76579d9e8bc7803
Ismai21k <ismailkabir170@gmail.com>
2025-01-27 07:10:50 +0100

    Initial submit

---
+++ todo/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponse
+
+
+# import todo form modles
+from .forms import TodoForm
+from .models import Todo
+
+# Create your views here.
+
+def home(request):
+    
+    item_list = Todo.objects.order_by('-date')
+    
+    if request.method == 'POST':
+        forms = TodoForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            messages.success(request, 'Todo Item added!!!')
+
+            return redirect('home')
+    else:
+        forms = TodoForm()
+    
+    page = {
+        'forms': forms,
+        'list': item_list,
+        'titles': 'TODO LIST',
+    }
+
+    return render(request, 'todo/index.html', page)
+        
+    #return HttpResponse(page)
+
+def remove(request, item_id):
+    item = Todo.objects.get(id=item_id)
+    item.delete()
+    messages.info(request, 'Todo Item removed!!!')
+    return redirect('home')
